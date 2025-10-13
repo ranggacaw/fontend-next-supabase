ce9e045f7118b14bb9e88b8fcced3b027e8380ac
Ranggacaw <ranggacahyaw@gmail.com>
2025-10-13 17:32:50 +0700

    first commit

---
+++ src/app/blog-list/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const BlogListPage = () => {
+  return (
+    <>
+      <div>BlogListPage</div>
+      <Button>Clickme</Button>
+    </>
+  )
+}
+
+export default BlogListPage
